a37afb04004c3a39aecc8f0a0f8d74efe97d9dce
sook1281 <sook1281naver.com>
2023-07-14 18:36:51 +0900

    first commit

---
+++ src/chap05/StringOperationExample.java
@@ -0,0 +1,30 @@
+package chap05;
+
+public class StringOperationExample {
+
+	/*
+     * Mutable (수정불가능한 데이터)
+     * Immutalbe (수정불가 데이터)
+      */
+	public static void main(String[] args) {
+       String str = "자바 프로그래밍";
+       
+       String str2 = str.replace("자바", "JAVA");
+       
+       System.out.println(str2.toLowerCase());
+       
+       System.out.println(str);
+       System.out.println(str2);
+       
+       System.out.println(str.substring(3));
+       System.out.println(str.substring(3, 5));
+       
+       System.out.println("   Hello   ");
+       System.out.println("   Hello   ".trim());
+
+
+
+       
+	}
+
+}
